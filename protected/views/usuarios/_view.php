ec113eb3f0524632e84c1cc33cb7bf593c6b4d39
Omar Peralta <ob.peralta03@gmail.com>
2016-06-10 15:27:59 -0700

    subiendo cambios en todo .

---
+++ protected/views/usuarios/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this UsuariosController */
+/* @var $data Usuarios */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('usuario')); ?>:</b>
+	<?php echo CHtml::encode($data->usuario); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('pwd')); ?>:</b>
+	<?php echo CHtml::encode($data->pwd); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tipo_usr')); ?>:</b>
+	<?php echo CHtml::encode($data->tipo_usr); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_usr')); ?>:</b>
+	<?php echo CHtml::encode($data->id_usr); ?>
+	<br />
+
+
+</div>
