b472f5799c8597de86bd60fdd426b4691fbff080
Rodolfo Robles <rm.rodolfo@live.com>
2016-06-11 10:01:55 -0700

    Added actions for webService

--- protected/controllers/WebServiceController.php
+++ protected/controllers/WebServiceController.php
@@ -1,36 +1,60 @@
 <?php
 
-class WebServiceController extends Controller
+class WebServiceController extends CController
 {
+	/*	Actions(WebServices)
+	 *	driver ( get driver user name and return status)
+	 *	getDataDriver ( get the Travel data to device )
+	 *	receiveData ( receive data from device to update tables on server )
+	 * 	
+	*/
 	public function actionIndex()
 	{
-		$this->render('index');
+		/*
+		*	definie variables for webService android app.
+		*	Post variables
+		*	Control variables:	Accion (ax), ApiKey (apiKey), 
+		*	Sensor variables:	Oxigen (Ox), PH (Ph), Temp (T2),
+		*	Position:			Lat (lt), Long (ln), Time (tm),
+		*	
+		*/
+
+		$ax = isset($_GET['ax'] )?$_GET['ax']:'0';
+		$apiKey = isset($_GET['apiKey'] )?$_GET['apiKey']:'0';
+		
+		$data = array(
+				'ax'=>$ax,
+				'apiKey'=>$apiKey,
+			);
+
+		$this->render('index', array('data'=>$data) );
 	}
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+    public function actionDriver(){
+    	$driver = isset($_GET['driver'])?$_GET['driver']:"0";
+    	//query
+    	if($driver > 10)
+    		$ax = array('Name'=>'Rodolfo','Status'=>'OK','SCode'=>'200');
+    	else
+    		$ax = array('Name'=>'NOVALID','Status'=>'X','SCode'=>'4BD');
+    	echo json_encode($ax);
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+    }
+
+    public function actionGetDataDriver(){
+    	$ax = isset($_GET['ax'] )?$_GET['ax']:'0';
+    	$apiKey = isset($_GET['apiKey'])?$_GET['apiKey']:"0";
+    	//query
+    	$rx = array('Name'=>'TRIP','Status'=>'OK','SCode'=>$ax,'ak'=>$apiKey);
+    	echo json_encode($rx);
+
+    }
+
+    public function actionReceiveData(){
+    	$dt=isset($_GET['dt'])?$_GET['dt']:"0";
+    	//query
+    	$sql = "insert into table (data) values (dt) ".$dt;
+    	echo $sql;
+    }
+    
 }
