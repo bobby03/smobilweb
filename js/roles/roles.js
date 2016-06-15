5bc906f1d8d6353f7aa2558a882d2dbe7b26b682
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-06-15 14:41:30 -0700

    cambios roles y usuarios

--- js/roles/roles.js
+++ js/roles/roles.js
@@ -1,8 +1,67 @@
-$(document).ready(function(){ 
-	$('a.delete img').click(function(){
-      		
-$.colorbox;
-
-	});
-
+$(document).ready(function()
+{ 
+    checkBox();
+    $('div.botonTodos').click(function()
+    {
+        if($(this).hasClass('clicked'))
+        {
+            $(this).removeClass('clicked');
+            $(this).text('Marcar');
+            $(this).parent().siblings('div').children('[type="checkbox"]').prop('checked', false);
+        }
+        else
+        {
+            $(this).addClass('clicked');
+            $(this).text('Desmarcar');
+            $(this).parent().siblings('div').children('[type="checkbox"]').prop('checked', true);
+        }
+    });
+    $('[type="checkbox"]').click(function()
+    {
+        var total = 0;
+        if($(this).is(':checked'))
+            total++;
+        $(this).parent().siblings('div').children('[type="checkbox"]').each(function()
+        {
+            if($(this).is(':checked'))
+                total++;
+        });
+        if(total == 0)
+        {
+            $(this).parent().siblings('div').children('div.botonTodos').removeClass('clicked');
+            $(this).parent().siblings('div').children('div.botonTodos').text('Marcar'); 
+        }
+        if(total == 4)
+        {
+            $(this).parent().siblings('div').children('div.botonTodos').addClass('clicked');
+            $(this).parent().siblings('div').children('div.botonTodos').text('Desmarcar'); 
+        }
+    });
+    function checkBox()
+    {
+        $('div.separador').each(function()
+        {
+            var total = 0;
+            var cont = 0;
+            $(this).find('[type="checkbox"]').each(function()
+            {
+                if($(this).is(':checked'))
+                total++;
+                cont++;
+                if(cont == 4)
+                {
+                    if(total == 0)
+                    {
+                        $(this).parent().siblings('div').children('div.botonTodos').removeClass('clicked');
+                        $(this).parent().siblings('div').children('div.botonTodos').text('Marcar'); 
+                    }
+                    if(total == 4)
+                    {
+                        $(this).parent().siblings('div').children('div.botonTodos').addClass('clicked');
+                        $(this).parent().siblings('div').children('div.botonTodos').text('Desmarcar'); 
+                    }
+                }
+            });
+        });
+    }
 });
