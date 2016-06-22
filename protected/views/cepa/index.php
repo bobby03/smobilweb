15ec1fd2d59240ad06ea089d6631b1e5b2f92277
MidoriHeart <midori.wings@gmail.com>
2016-06-22 15:57:55 -0700

    menu css

--- protected/views/cepa/index.php
+++ protected/views/cepa/index.php
@@ -20,7 +20,7 @@ $this->menu=array(
 
 
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
+<div class="search-form" >
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 )); ?>
@@ -30,7 +30,7 @@ $this->menu=array(
 	'id'=>'cepa',
 	'dataProvider'=>$model->search(),
 	'summaryText'=> '',
-	
+	'filter'=>$model,
 	'columns'=>$model->adminSearch()
 )); ?>
 
