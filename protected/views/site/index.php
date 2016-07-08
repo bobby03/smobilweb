6c39a9eb5ac598a05d01fdc18079f1e161d1728a
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-07-08 08:36:08 -0700

    c

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,4 +3,10 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
+<div class="principal">
+    <div class="tabs">
+        <div data-id="1">Viajes</div>
+        <div data-id="2">Estaciones</div>
+    </div>
+    
+</div>
