ad6783b629e2b0d1fa45ed68a7f5ce7ffb3c1f48
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-07-15 09:03:37 -0700

    cambios

--- js/estacion/create.js
+++ js/estacion/create.js
@@ -13,23 +13,26 @@ $(document).ready(function()
     contar();
     $('.addTanque').click(function()
     {
-        var campo = $('.tanque[data-id="1"]').clone();
-        $('.allTanques').append(campo);
-        total = total + 1;
-        $('.allTanques').children().last().attr('data-id', total);
-        $('.allTanques').children().last().children('input[type="hidden"]').remove();
-        $('.allTanques').children().last().children('.row.cap').children('input')
-                .attr('name','Tanque[status]['+total+'][capacidad]')
-                .attr('id','Tanque_status_'+total+'_capacidad')
-                .removeAttr('disabled')
-                .val('');
-        $('.allTanques').children().last().children('.row.nom').children('input')
-                .attr('name','Tanque[status]['+total+'][nombre]')
-                .attr('id','Tanque_status_'+total+'_nombre')
-                .removeAttr('disabled')
-                .val('');
-        $('.allTanques').children().last().children('.row.sta').remove();
-        $('.allTanques').children().last().children('.row.act').remove();
+        if(total < 8)
+        {
+            var campo = $('.tanque[data-id="1"]').clone();
+            $('.allTanques').append(campo);
+            total = total + 1;
+            $('.allTanques').children().last().attr('data-id', total);
+            $('.allTanques').children().last().children('input[type="hidden"]').remove();
+            $('.allTanques').children().last().children('.row.cap').children('input')
+                    .attr('name','Tanque[status]['+total+'][capacidad]')
+                    .attr('id','Tanque_status_'+total+'_capacidad')
+                    .removeAttr('disabled')
+                    .val('');
+            $('.allTanques').children().last().children('.row.nom').children('input')
+                    .attr('name','Tanque[status]['+total+'][nombre]')
+                    .attr('id','Tanque_status_'+total+'_nombre')
+                    .removeAttr('disabled')
+                    .val('');
+            $('.allTanques').children().last().children('.row.sta').remove();
+            $('.allTanques').children().last().children('.row.act').remove();
+        }
     });
     $('.editarTanque').click(function()
     {
@@ -38,3 +41,4 @@ $(document).ready(function()
         $(this).remove();
     });
 });
+        
