4aba05213fca3aa980f5e7471e2bb0fe442b456f
Edwinache Herrera <ed.herrera92@gmail.com>
2016-08-04 12:56:36 -0600

    Especie Colorbox
    sadf

--- js/especie/especie.js
+++ js/especie/especie.js
@@ -19,7 +19,7 @@ $(document).ready(function()
             height:'200px',
             onComplete: function()
             {
-                ;
+                
                 $('.btnadd').click(function()
                 {
                     var especie = document.getElementById('ingesp').value;
@@ -47,7 +47,61 @@ $(document).ready(function()
         });
     });
 
-    
+    $('a.update img').click(function(evt)
+    {
+        evt.preventDefault();
+        var href = $(this).parent().attr('href');
+        var index1 = (href.lastIndexOf('/'))+1;
+        var index2 = href.length;
+        var id = href.substring(index1, index2);
+        var miHtml = '';
+        var nombre = $(this).parents('tr').eq(0).find('td').html();
+        console.log(nombre);
+        miHtml= '<div class="sub-content">\n\
+                <div class="title-content">Editar especie '+'</div>\n\
+                <div class="esp">Especie</div>\n\
+                <div class="separator-content"></div>\n\
+                <input id="ingesp" value="'+nombre+'" class="ingesp" type="text">\n\
+                <div class="botones-content">\n\
+                    <div class="btnadd">Agregar</div>\n\
+                </div>\n\
+        </div>';
+        $.colorbox(
+        {
+            html: miHtml,
+            width:'400px', 
+            height:'200px',
+            onComplete: function()
+            {
+                
+                $('.btnadd').click(function()
+                {
+                    var especie = document.getElementById('ingesp').value;
+
+                    $.ajax(
+                    {
+                        type: 'POST',
+                        url: '/especie/Update1',
+                        dataType: 'JSON', 
+                        data:
+                        {
+                            id:id,
+                            especie: especie
+                        },
+                        success: function(data)
+                        {
+                            $('#cboxClose').click();
+                             window.location = "especie";
+                        },
+                        error: function(a, b, c)
+                        {
+                            
+                        }
+                    });
+                });
+            }
+        });
+    });
     $('.items tbody tr').each(function()
     {       
         $(this).find('a.view').remove();
