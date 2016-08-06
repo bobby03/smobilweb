9aaf68f31c68b77a77a403a14b992469953e61fd
Edwinache Herrera <ed.herrera92@gmail.com>
2016-08-06 17:14:06 -0600

    ======
    Iguales en el main

--- protected/config/main.php
+++ protected/config/main.php
@@ -91,11 +91,11 @@ return array(
 				// uncomment the following to show log messages on web pages
 				
 
-				array(
+/*				array(
 					'class'=>'CWebLogRoute',
 				),
-				
-=======
+*/				
+
 			),
 		),
 	),
