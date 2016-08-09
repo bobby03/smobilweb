2381658eb6ab569168597dd156d16fc6f4f287a2
Edwinache Herrera <ed.herrera92@gmail.com>
2016-08-09 10:15:55 -0600

    Model add
    Registro camp
    camp sensado
    camp tanque

---
+++ protected/views/campSensado/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this CampSensadoController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->breadcrumbs=array(
+	'Camp Sensados',
+);
+
+$this->menu=array(
+	array('label'=>'Create CampSensado', 'url'=>array('create')),
+	array('label'=>'Manage CampSensado', 'url'=>array('admin')),
+);
+?>
+
+<h1>Camp Sensados</h1>
