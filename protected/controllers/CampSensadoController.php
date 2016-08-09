57170ffab59d97fbc02aa8f6ce7c64fe184f363e
Edwinache Herrera <ed.herrera92@gmail.com>
2016-08-09 15:55:59 -0600

    View

--- protected/controllers/CampSensadoController.php
+++ protected/controllers/CampSensadoController.php
@@ -223,12 +223,12 @@ class CampSensadoController extends Controller
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		//if(!isset($_GET['ajax']))
 		//	$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		$this->loadModel($id);
+			/*$model=$this->loadModel($id);
             $model = $this->loadModel($id);
             $model->activo = 0;
             $update = Yii::app()->db->createCommand()
-                    ->update('camp_sensado',$model->attributes,"id = ".(int)$id."");
-            echo json_encode(true);
+                    ->update('camp_sensado',$model->attributes,"id = ".(int)$id."");*/
+            echo json_encode('');
 	}
 
 	/**
