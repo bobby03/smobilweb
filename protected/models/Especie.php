424b14f97a60d9c4dde8536d89e3ca5a565d4b23
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-08-12 16:29:19 -0700

    cambios ne los botones

--- protected/models/Especie.php
+++ protected/models/Especie.php
@@ -27,6 +27,7 @@ class Especie extends CActiveRecord
 		return array(
 			array('nombre', 'required','message'=>'Campo obligatorio'),
 			array('nombre', 'length', 'max'=>100),
+			array('nombre', 'unique', 'message'=>'Ya existe un Rol con ese nombre'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, nombre', 'safe', 'on'=>'search'),
