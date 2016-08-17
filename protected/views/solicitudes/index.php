b6945302e7c62854cba3a40fdd2ffd3aa46fca9a
Rodolfo Robles <rodosel@@gmail.com>
2016-08-17 11:21:41 -0700

    Actualizado búsqueda Solicitudes

--- protected/views/solicitudes/index.php
+++ protected/views/solicitudes/index.php
@@ -13,7 +13,6 @@
 //$a=SolicitudesViaje::model()->findByAttributes(array(74));
 ?>
 <div class="principal">
-    <input type='hidden' id='idviaje'>
     <div class="tabs">
         <div class="tab select" data-id="1"><span>Sin asignar</span></div>
         <div class="tab" data-id="2"><span>Asignadas</span></div>
@@ -29,10 +28,7 @@
             <div class="agregar clientes"></div>
         </a>
     </div><!-- search-form -->
-    <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/solicitudes/create">
-        <div class="agregar solicitudes"></div>
-    </a>
-    </div>
+
     <?php $this->widget('zii.widgets.grid.CGridView', array
     (
         'id'=>'viaje1',
@@ -117,4 +113,3 @@
     ?>
     </div>
 </div>
-
