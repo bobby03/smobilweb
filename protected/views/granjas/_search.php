9ced50df69616b0e85d5ef4afd9fc7244f162269
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-08-24 16:16:09 -0700

    .

--- protected/views/granjas/_search.php
+++ protected/views/granjas/_search.php
@@ -10,14 +10,8 @@
 	'action'=>Yii::app()->createUrl($this->route),
 	'method'=>'get',
 )); ?>
-<<<<<<< HEAD
     <?php echo CHtml::dropDownList('searchDropDown', 'id', $model->getSearchGranjas(),array('empty' =>'Selecciona campo a buscar')); ?>
 	<div class="row hide" data-id="1">
-=======
-    <?php //echo CHtml::dropDownList('searchDropDown', 'id', $model->getSearchGranjas(),array('empty' =>'Selecciona Búsqueda')); ?>
-	<div class="row" data-id="1">
-		<?php echo $form->label($model,'nombre'); ?>
->>>>>>> b412a666db4f6bc404a924090d87af35c7300f51
 		<?php echo $form->textField($model,'nombre',array('size'=>60,'maxlength'=>100)); ?>
 	</div>
 	<div class="row hide" data-id="2">
@@ -26,18 +20,6 @@
 	<div class="row hide" data-id="3">
 		<?php echo $form->textField($model,'responsable',array('size'=>60,'maxlength'=>100)); ?>
 	</div>
-<<<<<<< HEAD
-        <div class="row buttons hide">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-=======
-	<div class="row hide" data-id="3">
-		<?php echo $form->label($model,'direccion'); ?>
-		<?php echo $form->textField($model,'direccion',array('size'=>60,'maxlength'=>100)); ?>
-	</div>
-
-
->>>>>>> b412a666db4f6bc404a924090d87af35c7300f51
 <?php $this->endWidget(); ?>
 
 </div><!-- search-form -->
